5f2000b4491ff17bd8d2b0672601b59f698f3705
Alexander <a.p.kobelev@gmail.com>
2023-09-10 16:24:06 +0300

    add custom component Context Provider

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import styles from './Home.module.css';
+import AuthContext from '../../context/auth-context';
 
 const Home = (props) => {
+  const cxt = useContext(AuthContext);
   return (
     <Card className={styles.home}>
       <h1>Рады Вас Видеть Снова!</h1>
-      <Button onClick={props.onLogout}>Выход</Button>
+      <Button onClick={cxt.onLogout}>Выход</Button>
     </Card>
   );
 };
